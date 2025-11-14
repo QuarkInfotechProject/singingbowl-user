a43f6aa7f680e20eb0e1a29f76a21e0590675023
Amrit Bhusal <45762946+AmritBhusal@users.noreply.github.com>
2025-11-14 12:05:29 +0545

    ecosystem added

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+      {
+        name: 'user.singingbowlvillagenepal.com',
+        script: 'npm',
+        exec_mode: "fork",  // Enables clustering
+        instances: "1", // Change to "max" to use all available cores        
+        args: 'run start',
+        watch: false, // Set to true if you want to watch file changes
+        max_memory_restart: "512M",
+        env: {
+          NODE_ENV: 'production',
+        },
+      },
+    ],
+  };
