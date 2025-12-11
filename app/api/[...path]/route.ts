f38ef5b658b4fae38040e5575f1368f15e084b13
Amrit Bhusal <45762946+AmritBhusal@users.noreply.github.com>
2025-12-11 13:20:41 +0545

    code updated

--- app/api/[...path]/route.ts
+++ app/api/[...path]/route.ts
@@ -4,7 +4,7 @@ import { cookies } from "next/headers";
 
 const apiClient = axios.create({
   baseURL: process.env.BASE_URL,
-  timeout: 10000,
+  timeout: 60000,
   headers: {
     "Content-Type": "application/json",
   },
--- lib/apiItems.ts
+++ lib/apiItems.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "/api",
+    timeout: 60000,
     headers: {
         "Content-Type": "application/json",
     },
